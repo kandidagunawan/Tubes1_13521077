8211851d84fc651a1b00c86b233352da4d59fe54
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-09-13 12:39:07 +0700

    objective function

--- src/Bot.java
+++ src/Bot.java
@@ -1,4 +1,25 @@
+import java.awt.*;
+import javafx.scene.control.Button;
+
 public class Bot {
+
+    public int objectiveFunction(int[] currentPost, Button[][] button) {
+        // ngitung berapa banyak jumlah lawan yang bisa keganti dari posisi kita sekarang
+        // initiates position from up left right bottom
+        int[][] positions = {{currentPost[0], currentPost[1] + 1}, {currentPost[0]- 1, currentPost[1]}, {currentPost[0] + 1, currentPost[1]}, {currentPost[0], currentPost[1] - 1}};
+
+        int count = 0;
+        for (int[] post : positions) {
+            int i = post[0];
+            int j = post[1];
+            if (button[i][j].getText().equals("X")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public int[] move() {
         // create random move
         return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
